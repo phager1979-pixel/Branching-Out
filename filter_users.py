3746f0f2d9c5c5a1459446060f5e9a7e30edba10
phager1979-pixel <phager1979@gmail.com>
2026-09-21 13:44:13 +0200

    Add Python script to filter users by age

--- filter_users.py
+++ filter_users.py
@@ -1,6 +1,16 @@
 import json
 
 
+def filter_users_by_age(age):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    # Filtert die Nutzer, deren Alter genau übereinstimmt (als Zahl verglichen)
+    filtered_users = [user for user in users if user["age"] == int(age)]
+
+    for user in filtered_users:
+        print(user)
+
 def filter_users_by_name(name):
     with open("users.json", "r") as file:
         users = json.load(file)
@@ -12,10 +22,15 @@ def filter_users_by_name(name):
 
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (Currently, only 'name' and 'age'are supported): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
-    else:
-        print("Filtering by that option is not yet supported.")
+
+    if filter_option == "age":
+        age_to_search = input("Enter an age to filter users: ").strip()
+        filter_users_by_age(age_to_search)
+
+else:
+    print("Filtering by that option is not yet supported.")
